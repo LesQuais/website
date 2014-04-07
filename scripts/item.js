64689e706fa20c0149731cfe950d7d68efc37fdf
Matti Schneider <hi@mattischneider.fr>
2014-04-07 20:32:49 +0200

    Correct generic computed positioning

--- scripts/item.js
+++ scripts/item.js
@@ -1,12 +1,13 @@
 document.body.onload = function() {
 	var bg		= document.getElementById('bg'),
 		boat	= document.getElementById('boat'),
+		above	= document.getElementById('above')
 		spacer	= document.getElementById('spacer');
 
-	spacer.style.height = document.height + 'px';
+	spacer.style.height = window.innerHeight + 'px';
 
 	window.addEventListener('scroll', function() {
-		bg.style.top = document.height - bg.height - (document.body.scrollTop / 1.5) + 'px';
-		boat.style.bottom = ((document.body.scrollTop - spacer.offsetTop) / 1.25) + 'px';
+		bg.style.top = ((above.clientHeight - document.body.scrollTop) / 1.5) + 'px';
+		boat.style.bottom = ((document.body.scrollTop - spacer.offsetTop) / 1.25) - 120 + 'px';
 	});
 }
