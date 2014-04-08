a6af81b809913483bca4d3ae97ba5c1d1b18d50b
Matti Schneider <hi@mattischneider.fr>
2014-04-08 13:09:26 +0200

    Stop requesting additional downwards content in boat images

--- scripts/item.js
+++ scripts/item.js
@@ -8,7 +8,7 @@ document.body.onload = function() {
 
 	window.addEventListener('scroll', function() {
 		bg.style.top = ((above.clientHeight - document.body.scrollTop) / 1.5) + 'px';
-		boat.style.bottom = ((document.body.scrollTop - spacer.offsetTop) / 1.25) - 120 + 'px';
+		boat.style.bottom = ((document.body.scrollTop - spacer.offsetTop) / 1.25) + 'px';
 	});
 
 	window.scrollTo(0, spacer.offsetTop);
