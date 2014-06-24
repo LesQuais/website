b246f7ee95b568e53713fd757614a8bbfd857f99
Matti Schneider <hi@mattischneider.fr>
2014-06-24 18:19:46 +0200

    Include postcard images at semantic position
    Correct parallax effect for bg over boats

--- scripts/item.js
+++ scripts/item.js
@@ -1,14 +1,14 @@
 document.body.onload = function() {
-	var bg		= document.getElementById('bg'),
-		boat	= document.getElementById('boat'),
+	var under	= document.getElementById('under'),
+		over	= document.getElementById('over'),
 		above	= document.getElementById('above')
 		spacer	= document.getElementById('spacer');
 
 	spacer.style.height = window.innerHeight + 'px';
 
 	window.addEventListener('scroll', function() {
-		bg.style.top = ((above.clientHeight - window.scrollY) / 1.5) + 'px';
-		boat.style.bottom = ((window.scrollY - spacer.offsetTop) / 1.25) + 'px';
+		under.style.top = ((above.clientHeight - window.scrollY) / 1.5) + 'px';
+		over.style.top = ((above.clientHeight - window.scrollY) / 1.25) + 'px';
 	});
 
 	window.scrollTo(0, spacer.offsetTop);
