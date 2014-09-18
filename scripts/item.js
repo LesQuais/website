dca6dec67340cb9d942eb13ccb8282e4e401eaff
Matti Schneider <hi@mattischneider.fr>
2014-09-18 15:27:04 +0200

    Center postcard

--- scripts/item.js
+++ scripts/item.js
@@ -1,11 +1,14 @@
 window.onload = function() {
 	var under	= document.getElementById('under'),
 		over	= document.getElementById('over'),
-		above	= document.getElementById('above');
+		above	= document.getElementById('above'),
+		pixelsLeakingBelow = under.clientHeight - window.innerHeight;
+
+	var offset	= (pixelsLeakingBelow > 0 ? pixelsLeakingBelow / 2 : 0);
 
 	function scrollHandler() {
-		under.style.top	= ((above.clientHeight - window.scrollY) / 1.5) + 'px';
-		over.style.top	= ((above.clientHeight - window.scrollY) / 1.25) + 'px';
+		under.style.top	= ((above.clientHeight - window.scrollY) / 1.5 - offset) + 'px';
+		over.style.top	= ((above.clientHeight - window.scrollY) / 1.25 - offset) + 'px';
 	}
 
 	window.addEventListener('scroll', scrollHandler);
